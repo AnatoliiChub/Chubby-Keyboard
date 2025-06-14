7922202d2ad4d5ecb85b720bc317ea3b150ffe69
Anatolii Chub <anatoliy.chub.dev@gmail.com>
2025-06-14 16:35:01 +0100

    Edge to edge bottom inset fixed.

--- app/src/main/java/com/chubbykeyboard/ui/view/keyboard/ChubbyKeyboard.kt
+++ app/src/main/java/com/chubbykeyboard/ui/view/keyboard/ChubbyKeyboard.kt
@@ -3,24 +3,25 @@ package com.chubbykeyboard.ui.view.keyboard
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.chubbykeyboard.data.parser.KeyMatrix
 import com.chubbykeyboard.domain.keyboard.KeyBoardState
 import com.chubbykeyboard.ui.theme.ChubbyKeyboardTheme
-import com.chubbykeyboard.ui.theme.KEYBOARD_HEIGHT
 import com.chubbykeyboard.ui.theme.KEYBOARD_VERTICAL_PADDING
 import com.chubbykeyboard.ui.theme.PROGRESS_BAR_SIZE
 
 @Composable
 fun ChubbyKeyboard(
+    bottomPadding: MutableState<Int>,
     viewModel: ChubbyKeyboardViewModel
 ) {
     val state = viewModel.uiState.collectAsStateWithLifecycle()
@@ -28,17 +29,21 @@ fun ChubbyKeyboard(
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(KEYBOARD_HEIGHT)
                 .background(MaterialTheme.colorScheme.background),
             contentAlignment = Alignment.Center
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = KEYBOARD_VERTICAL_PADDING)
+                    .padding(bottom = KEYBOARD_VERTICAL_PADDING + bottomPadding.value.dp)
             ) {
                 when (val content = state.value) {
-                    KeyBoardState.Loading -> CircularProgressIndicator(modifier = Modifier.size(PROGRESS_BAR_SIZE))
+                    KeyBoardState.Loading -> CircularProgressIndicator(
+                        modifier = Modifier.size(
+                            PROGRESS_BAR_SIZE
+                        )
+                    )
+
                     is KeyBoardState.Content -> {
                         if (content.keyMatrix is KeyMatrix.NumPadMatrix) {
                             Numpad(content, viewModel.router)
--- app/src/main/java/com/chubbykeyboard/ui/view/keyboard/ChubbyKeyboardView.kt
+++ app/src/main/java/com/chubbykeyboard/ui/view/keyboard/ChubbyKeyboardView.kt
@@ -2,15 +2,39 @@ package com.chubbykeyboard.ui.view.keyboard
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.AbstractComposeView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 @SuppressLint("ViewConstructor")
 class ChubbyKeyboardView(context: Context, private val viewModel: ChubbyKeyboardViewModel) :
     AbstractComposeView(context) {
 
+    private var bottomPadding: MutableState<Int> = mutableIntStateOf(0)
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        launchInsetListener()
+    }
+
+    private fun launchInsetListener() {
+        ViewCompat.setOnApplyWindowInsetsListener(this) { view, windowInsets ->
+            val density = context.resources.displayMetrics.density
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            bottomPadding.value = insets.bottom / density.toInt()
+            WindowInsetsCompat.CONSUMED
+        }
+    }
+
     @Composable
     override fun Content() {
-        ChubbyKeyboard(viewModel)
+        ChubbyKeyboard(bottomPadding, viewModel)
     }
 }
